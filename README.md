45292c794a08f2b2069f742c26c3b530cecc93f7
J.J. Moncus <jj.moncus@gartner.com>
2023-05-05 17:39:42 -0400

    adding hexsticker

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# noodles
+# noodles <img src="inst/figures/noodles-hexsticker.jpg" align="right" height="139" />
 
 <!-- badges: start -->
 
---
+++ inst/figures/hecsticker.R
@@ -0,0 +1,26 @@
+
+sticker("inst/figures/noodle-cartoon.jpg", 
+        
+        # package name
+        package="noodles", 
+        p_size = 16, 
+        p_x = 1,
+        p_y = 1.5,
+        p_color = "#390304",
+        
+        # hex border
+        h_size = 1,
+        
+        #background color
+        h_fill = "#A4BDE5",
+        h_color = "#390304",
+        
+        # cartoon
+        s_x = 1, 
+        s_y = .85, 
+        s_width = .55,
+        s_height = .4,
+        
+        spotlight = FALSE,
+        # export to
+        filename = "inst/figures/noodles-hexsticker.jpg")
