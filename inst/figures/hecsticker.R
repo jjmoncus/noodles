86a39f95ffa096d34fae5a9635f1d2baa0c21e3c
J.J. Moncus <jj.moncus@gartner.com>
2025-04-11 15:56:41 -0400

    fixed hexsticker whitespace (just needed to change to png)

--- inst/figures/hecsticker.R
+++ inst/figures/hecsticker.R
@@ -1,26 +1,31 @@
+s <- sticker(
+  "inst/figures/noodles cartoon.png",
 
-sticker("inst/figures/noodle-cartoon.jpg", 
-        
-        # package name
-        package="noodles", 
-        p_size = 16, 
-        p_x = 1,
-        p_y = 1.5,
-        p_color = "#390304",
-        
-        # hex border
-        h_size = 1,
-        
-        #background color
-        h_fill = "#A4BDE5",
-        h_color = "#390304",
-        
-        # cartoon
-        s_x = 1, 
-        s_y = .85, 
-        s_width = .55,
-        s_height = .4,
-        
-        spotlight = FALSE,
-        # export to
-        filename = "inst/figures/noodles-hexsticker.jpg")
+  # package name
+  package = "noodles",
+  p_size = 18,
+  p_x = 1,
+  p_y = 1.55,
+  p_color = "#390304",
+
+  # hex border
+  h_size = 1,
+
+  #background color
+  h_fill = "#F6E3B0",
+  h_color = "#554c2e",
+
+  # cartoon
+  s_x = 1,
+  s_y = .95,
+  s_width = .55,
+  s_height = .4,
+
+  spotlight = FALSE,
+  white_around_sticker = FALSE,
+  # export to
+  filename = "inst/figures/noodles-hexsticker.png"
+)
+
+# plot(s)
+
