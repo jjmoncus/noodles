4dbf14a14b0a5a0f7a63d953aed1866ac0fd0e0a
J.J. Moncus <jj.moncus@gartner.com>
2025-04-11 20:16:24 -0400

    removing g_colors

--- R/g_colors.R
+++
@@ -1,9 +0,0 @@
-
-#' Official Gartner color palette
-#'
-#' These colors were last checked with the Gartner Brand Center in
-#' November 2021.
-#'
-#' @format ## `g_colors`
-#'
-#' @source Gartner Brand Center
